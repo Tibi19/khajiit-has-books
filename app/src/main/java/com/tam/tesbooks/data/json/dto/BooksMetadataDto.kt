ca020e2ae9632a6ac1f99061f9068ab152fdf706
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2022-09-08 19:16:37 +0300

    Fix Books Metadata DTO not finding field

--- app/src/main/java/com/tam/tesbooks/data/json/dto/BooksMetadataDto.kt
+++ app/src/main/java/com/tam/tesbooks/data/json/dto/BooksMetadataDto.kt
@@ -1,9 +1,11 @@
 package com.tam.tesbooks.data.json.dto
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import com.tam.tesbooks.domain.model.metadata.BookMetadata
 
 @JsonClass(generateAdapter = true)
 data class BooksMetadataDto(
-    val booksMetadata: List<BookMetadata>
+    @Json(name = "books_metadata")
+    val booksMetadata: List<BookMetadataDto>
 )
