2fcc16a713d9668b07bc6f573a8f05cbf8a6ab71
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2022-09-12 13:29:39 +0300

    Add constants for books size limit query default

--- app/src/main/java/com/tam/tesbooks/util/Constants.kt
+++ app/src/main/java/com/tam/tesbooks/util/Constants.kt
@@ -4,6 +4,7 @@ const val DATABASE_BOOK_INFO = "database_book_info"
 const val TABLE_BOOK_LIST = "table_book_list"
 const val TABLE_BOOK_SAVE = "table_book_save"
 const val TABLE_BOOKMARK = "table_bookmark"
+const val TABLE_BOOK_METADATA = "table_book_metadata"
 
 const val INSTANCE_IO_DISPATCHER = "io_dispatcher"
 const val INSTANCE_DEFAULT_DISPATCHER = "default_dispatcher"
@@ -13,3 +14,6 @@ const val PATH_BOOKS_METADATA = "books_metadata.json"
 const val PATH_BOOKS_CATEGORIES = "books_categories.json"
 const val PATH_BOOKS_TAGS = "books_tags.json"
 const val PATH_BOOK_TEXTS_FOLDER = "book_texts/"
+
+const val SIZE_BOOK_METADATAS = 5395
+const val LIMIT_ROOM_QUERY_DEFAULT = 20
