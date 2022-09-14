7c3be60bf3c08aa442715f5b9cb663603a2d247c
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2022-09-14 14:22:33 +0300

    Remove unneeded dao methods

--- app/src/main/java/com/tam/tesbooks/data/room/dao/BookMetadataDao.kt
+++ app/src/main/java/com/tam/tesbooks/data/room/dao/BookMetadataDao.kt
@@ -18,66 +18,10 @@ interface BookMetadataDao {
     @Query("SELECT * FROM table_book_metadata")
     suspend fun getMetadatas(): List<BookMetadataEntity>
 
-    @Query("SELECT * FROM table_book_metadata WHERE id IN (:bookIds)")
-    suspend fun getMetadatasWithIds(bookIds: List<Int>): List<BookMetadataEntity>
-
     @Query("SELECT COUNT(id) FROM table_book_metadata")
     suspend fun getMetadatasCount(): Int
 
-    @Query("SELECT MAX(id) FROM table_book_metadata")
-    suspend fun getMaxMetadataId(): Int
-
-    @Query(
-        """
-            SELECT * FROM table_book_metadata 
-            ORDER BY RANDOM() 
-            LIMIT :limit
-        """
-    )
-    suspend fun getRandomMetadatas(limit: Int = LIMIT_ROOM_QUERY_DEFAULT): List<BookMetadataEntity>
-
-    @Query(
-        """
-            SELECT * FROM table_book_metadata 
-            WHERE id NOT IN (:exceptIds) 
-            ORDER BY RANDOM() 
-            LIMIT :limit
-        """
-    )
-    suspend fun getRandomMetadatasExcept(exceptIds: List<Int>, limit: Int = LIMIT_ROOM_QUERY_DEFAULT): List<BookMetadataEntity>
-
-    @Query(
-        """
-            SELECT * FROM table_book_metadata
-            ORDER BY 
-                CASE WHEN :isReversed = 0 THEN textSize END DESC,
-                CASE WHEN :isReversed = 1 THEN textSize END ASC
-            LIMIT :limit
-        """
-    )
-    suspend fun getMetadatasBySize(isReversed: Boolean = false, limit: Int = LIMIT_ROOM_QUERY_DEFAULT): List<BookMetadataEntity>
-
-    @Query(
-        """
-            SELECT * FROM table_book_metadata 
-            WHERE textSize < :textSize
-            ORDER BY textSize DESC 
-            LIMIT :limit
-        """
-    )
-    suspend fun getMetadatasBeforeSize(textSize: Int, limit: Int = LIMIT_ROOM_QUERY_DEFAULT): List<BookMetadataEntity>
-
-    @Query(
-        """
-            SELECT * FROM table_book_metadata 
-            WHERE textSize > :textSize 
-            ORDER BY textSize ASC 
-            LIMIT :limit
-        """
-    )
-    suspend fun getMetadatasAfterSize(textSize: Int, limit: Int = LIMIT_ROOM_QUERY_DEFAULT): List<BookMetadataEntity>
-
     @RawQuery(observedEntities = [BookMetadataEntity::class])
-    suspend fun getWithQuery(query: SupportSQLiteQuery): List<BookMetadataEntity>
+    suspend fun getMetadatasWithQuery(query: SupportSQLiteQuery): List<BookMetadataEntity>
 
 }
