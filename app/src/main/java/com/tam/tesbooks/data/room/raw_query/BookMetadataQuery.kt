c44a6757332f34e73c980cf23f4cbf8b97dc749c
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2022-09-14 14:23:50 +0300

    Add filter, search, sort, order by date added

--- app/src/main/java/com/tam/tesbooks/data/room/raw_query/BookMetadataQuery.kt
+++ app/src/main/java/com/tam/tesbooks/data/room/raw_query/BookMetadataQuery.kt
@@ -2,10 +2,12 @@ package com.tam.tesbooks.data.room.raw_query
 
 import androidx.sqlite.db.SimpleSQLiteQuery
 import androidx.sqlite.db.SupportSQLiteQuery
-import com.tam.tesbooks.domain.model.listing_modifier.LibraryFilter
-import com.tam.tesbooks.domain.model.listing_modifier.LibraryOrder
-import com.tam.tesbooks.domain.model.listing_modifier.Order
+import com.tam.tesbooks.domain.model.listing_modifier.*
 import com.tam.tesbooks.util.LIMIT_ROOM_QUERY_DEFAULT
+import com.tam.tesbooks.util.TABLE_BOOK_METADATA
+import com.tam.tesbooks.util.TABLE_BOOK_SAVE
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 class BookMetadataQuery(val query: String) {
 
@@ -21,7 +23,8 @@ class BookMetadataQuery(val query: String) {
     class Builder {
 
         companion object {
-            const val TABLE = "table_book_metadata"
+            const val TABLE = TABLE_BOOK_METADATA
+            const val TABLE_SAVES = TABLE_BOOK_SAVE
         }
 
         private var select: String = "SELECT *"
@@ -36,6 +39,7 @@ class BookMetadataQuery(val query: String) {
 
         private var order: Order? = null
         private var isReversed: Boolean = false
+        private lateinit var sort: Sort
 
         private lateinit var column: String
         private lateinit var comparator: String
@@ -53,23 +57,43 @@ class BookMetadataQuery(val query: String) {
             when(order) {
                 Order.TITLE -> orderByTitle()
                 Order.CATEGORY -> orderByCategory()
-                Order.AUTHOR_NAME -> orderByAuthor()
-                Order.AUTHOR_COUNT -> orderByAuthorCount()
+                Order.AUTHOR_NAME -> orderByAuthorName()
                 Order.SIZE -> orderBySize()
                 else -> orderRandomly()
             }
         }
 
         private fun setColumn(column: String) {
-            if(this.column.isNotEmpty()) return
+            if(this::column.isInitialized && this.column.isNotEmpty()) return
             this.column = column
         }
 
-        fun after(value: Int) = after(value.toString())
-
-        fun after(value: String) {
+        /**
+         * Can be used in conjunction with ordering or sorting by size.
+         * Otherwise use except(exceptIds) to omit elements that are not needed.
+         */
+        fun after(value: Int) {
             addNewWhereClause()
             where += "$column $comparator $value"
+        }
+
+        /**
+         * Can be used in conjunction with ordering or sorting by title.
+         * Otherwise use except(exceptIds) to omit elements that are not needed.
+         */
+        fun after(value: String) {
+            addNewWhereClause()
+            where += "$column $comparator \"$value\""
+        }
+
+        /**
+         * Can be used in conjunction with sorting by date added.
+         * Otherwise use except(exceptIds) to omit elements that are not needed.
+         */
+        fun after(date: LocalDateTime) {
+            addNewWhereClause()
+            val timestamp = date.atZone(ZoneId.systemDefault()).toEpochSecond()
+            where += "$column $comparator $timestamp"
         }
 
         private fun addNewWhereClause() {
@@ -77,30 +101,42 @@ class BookMetadataQuery(val query: String) {
         }
 
         private fun orderByTitle() {
-            TODO("Not yet implemented")
+            setColumn("title")
+            comparator = if(isReversed) "<" else ">"
+            orderBy = "ORDER BY $column"
+            orderAs = if(isReversed) "DESC" else "ASC"
         }
 
         private fun orderByCategory() {
-            TODO("Not yet implemented")
+            setColumn("category")
+
+            orderAs = if(isReversed) "DESC" else "ASC"
+            orderBy = "ORDER BY CASE $column"
+            categoriesOrder.forEachIndexed { i, category ->
+                orderBy += " WHEN \'$category\' THEN $i"
+            }
+            orderBy += " END"
         }
 
-        private fun orderByAuthor() {
-            TODO("Not yet implemented")
-        }
-
-        private fun orderByAuthorCount() {
-            TODO("Not yet implemented")
+        private fun orderByAuthorName() {
+            setColumn("author")
+            comparator = if(isReversed) "<" else ">"
+            orderBy = "ORDER BY $column"
+            orderAs = if(isReversed) "DESC" else "ASC"
         }
 
         private fun orderBySize() {
             setColumn("textSize")
+
+            // Default ordering for size should be from biggest to lowest
             comparator = if(isReversed) ">" else "<"
+            orderAs = if(isReversed) "ASC" else "DESC"
+
             orderBy = "ORDER BY $column"
-            orderAs = if (isReversed) "ASC" else "DESC"
         }
 
         private fun orderRandomly() {
-            TODO("Not yet implemented")
+            orderBy = "ORDER BY RANDOM()"
         }
 
         fun except(exceptIds: List<Int>) {
@@ -109,8 +145,91 @@ class BookMetadataQuery(val query: String) {
             where += "id NOT IN ($exceptIdsString)"
         }
 
-        fun filter(libraryFilter: LibraryFilter) {
+        fun search(searchQuery: String) {
+            val likeQuery = "LIKE '%' || LOWER('$searchQuery') || '%'"
+            addNewWhereClause()
+            where += "(LOWER(title) $likeQuery OR "
+            where += "LOWER(author) $likeQuery OR "
+            where += "LOWER(description) $likeQuery)"
+        }
 
+        fun filter(libraryFilter: LibraryFilter) {
+            if(libraryFilter.tagFilters.isNotEmpty()) {
+                filterTags(libraryFilter.tagFilters)
+            }
+            if(libraryFilter.categoryFilters.isNotEmpty()) {
+                filterCategories(libraryFilter.categoryFilters)
+            }
+            if(libraryFilter.isExcludingAnonymous) {
+                filterAnonymous()
+            }
+        }
+
+        private fun filterTags(tags: List<String>) {
+            addNewWhereClause()
+            where += "("
+            tags.forEachIndexed { i, tag ->
+                where += "LOWER(tags) LIKE '%' || LOWER('$tag') || '%'"
+                if (i < tags.size - 1) {
+                    where += " AND "
+                }
+            }
+            where += ")"
+        }
+
+        private fun filterCategories(categories: List<String>) {
+            addNewWhereClause()
+            where += "("
+            categories.forEachIndexed { i, category ->
+                where += "LOWER(tags) LIKE '%' || LOWER('$category') || '%'"
+                if (i < categories.size - 1) {
+                    where += " AND "
+                }
+            }
+            where += ")"
+        }
+
+        private fun filterAnonymous() {
+            addNewWhereClause()
+            where += "author != 'Anonymous'"
+        }
+
+        fun fromIds(ids: List<Int>) {
+            addNewWhereClause()
+            val idsString = ids.joinToString(", ")
+            where += "id IN ($idsString)"
+        }
+
+        fun fromBookList(bookListId: Int) {
+            from = "FROM $TABLE_SAVES INNER JOIN $TABLE ON $TABLE_SAVES.bookId = $TABLE.id"
+            addNewWhereClause()
+            where += "bookListId = $bookListId"
+        }
+
+        fun sortList(bookListSort: BookListSort) {
+            isReversed = bookListSort.isReversed
+            sort = bookListSort.sortBy
+            when(sort) {
+                Sort.DATE_ADDED -> orderByDateAdded()
+                Sort.SIZE -> orderBySize()
+                Sort.TITLE -> orderByTitle()
+            }
+        }
+
+        private fun orderByDateAdded() {
+            setColumn("dateTimeSaved")
+            orderBy = "ORDER BY $column"
+            // Default ordering for date added should be from recent to old
+            comparator = if(isReversed) ">" else "<"
+            orderAs = if(isReversed) "ASC" else "DESC"
+        }
+
+        fun limit(limitElements: Int?) {
+            if(limitElements == null || limitElements <= 0) {
+                limit = ""
+                return
+            }
+            limit = "LIMIT $limitElements"
         }
 
     }
