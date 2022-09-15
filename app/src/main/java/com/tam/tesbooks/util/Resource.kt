862e9c3312aeb497d4f9d5a7cff149bdc1f7fa6a
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2022-09-15 18:41:35 +0300

    Add helper functions for Resource

--- app/src/main/java/com/tam/tesbooks/util/Resource.kt
+++ app/src/main/java/com/tam/tesbooks/util/Resource.kt
@@ -1,7 +1,30 @@
 package com.tam.tesbooks.util
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T?): Resource<T>(data)
+    class Success<T>(data: T? = null): Resource<T>(data)
     class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
     class Loading<T>(val isLoading: Boolean = true): Resource<T>(null)
+
+    inline fun onError(doOnError: (message: String?) -> Unit) {
+        if(this is Error) {
+            doOnError(message)
+        }
+    }
+
+    inline fun onSuccess(doOnSuccess: (data: T?) -> Unit) {
+        if(this is Success) {
+            doOnSuccess(data)
+        }
+    }
+
+    inline fun onResource(
+        onSuccess: (data: T?) -> Unit,
+        onError: (message: String?) -> Unit,
+        onLoading: (isLoading: Boolean) -> Unit = {}
+    ) =
+        when(this) {
+            is Success -> onSuccess(data)
+            is Error -> onError(message)
+            is Loading -> onLoading(isLoading)
+        }
 }
