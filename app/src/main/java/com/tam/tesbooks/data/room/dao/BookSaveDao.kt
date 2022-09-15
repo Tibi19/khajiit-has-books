d09127e49ec74d7553ba65cc000d6f831d9e9a06
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2022-09-15 18:41:19 +0300

    Fix DAOs not being able to interpret date

--- app/src/main/java/com/tam/tesbooks/data/room/dao/BookSaveDao.kt
+++ app/src/main/java/com/tam/tesbooks/data/room/dao/BookSaveDao.kt
@@ -3,6 +3,8 @@ package com.tam.tesbooks.data.room.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.TypeConverters
+import com.tam.tesbooks.data.room.converter.LocalDateTimeConverter
 import com.tam.tesbooks.data.room.entity.BookSaveEntity
 import java.time.LocalDateTime
 
@@ -15,6 +17,7 @@ interface BookSaveDao {
     @Query("DELETE FROM table_book_save WHERE bookId = :bookId AND bookListId = :bookListId")
     suspend fun deleteBookSave(bookId: Int, bookListId: Int)
 
+    @TypeConverters(LocalDateTimeConverter::class)
     @Query("SELECT dateTimeSaved FROM table_book_save WHERE bookId = :bookId AND bookListId = :bookListId LIMIT 1")
     suspend fun getDateTimeSaved(bookId: Int, bookListId: Int): LocalDateTime
 
--- app/src/main/java/com/tam/tesbooks/data/room/dao/BookmarkDao.kt
+++ app/src/main/java/com/tam/tesbooks/data/room/dao/BookmarkDao.kt
@@ -3,8 +3,9 @@ package com.tam.tesbooks.data.room.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.TypeConverters
+import com.tam.tesbooks.data.room.converter.LocalDateTimeConverter
 import com.tam.tesbooks.data.room.entity.BookmarkEntity
-import com.tam.tesbooks.domain.model.bookmark.Bookmark
 import com.tam.tesbooks.util.LIMIT_ROOM_QUERY_DEFAULT
 import java.time.LocalDateTime
 
@@ -17,6 +18,7 @@ interface BookmarkDao {
     @Query("DELETE FROM table_bookmark WHERE id = :bookmarkId")
     suspend fun deleteBookmark(bookmarkId: Int)
 
+    @TypeConverters(LocalDateTimeConverter::class)
     @Query("SELECT * FROM table_bookmark WHERE dateTimeAdded < :before ORDER BY dateTimeAdded DESC LIMIT :limitElements")
     suspend fun getBookmarks(
         before: LocalDateTime = LocalDateTime.now(),
