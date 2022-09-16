fc9c0abf9a3d86728245bfddd2e4e23ae2ad9ba1
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2022-09-16 13:36:29 +0300

    Add content parsing for different book paragraphs

--- app/src/main/java/com/tam/tesbooks/domain/model/book/ImageParagraph.kt
+++ app/src/main/java/com/tam/tesbooks/domain/model/book/ImageParagraph.kt
@@ -1,12 +1,16 @@
 package com.tam.tesbooks.domain.model.book
 
+import com.tam.tesbooks.util.FALLBACK_URL_NO_IMAGE
+import com.tam.tesbooks.util.getImageUrlRegex
+
 class ImageParagraph(bookId: Int, position: Int, content: String) : BookParagraph(bookId, position, content) {
 
     lateinit var imageUrl: String
         private set
 
     override fun parseContent() {
-        TODO("Not yet implemented")
+        val urlRegex = getImageUrlRegex()
+        imageUrl = urlRegex.find(content)?.value ?: FALLBACK_URL_NO_IMAGE
     }
 
 }
--- app/src/main/java/com/tam/tesbooks/domain/model/book/TextParagraph.kt
+++ app/src/main/java/com/tam/tesbooks/domain/model/book/TextParagraph.kt
@@ -1,12 +1,29 @@
 package com.tam.tesbooks.domain.model.book
 
+import com.tam.tesbooks.util.getTagsToTextAttributes
+import com.tam.tesbooks.util.getTextTagsRegex
+
 class TextParagraph(bookId: Int, position: Int, content: String): BookParagraph(bookId, position, content) {
 
     private val _attributes: MutableList<TextAttribute> = mutableListOf()
     val attributes: List<TextAttribute> = _attributes
 
+    lateinit var text: String
+        private set
+
     override fun parseContent() {
-        TODO("Not yet implemented")
+        parseCleanText()
+        parseAttributes()
     }
+
+    private fun parseCleanText() {
+        text = content.replace(getTextTagsRegex(), "")
+    }
+
+    private fun parseAttributes() =
+        getTagsToTextAttributes().forEach { textToAttribute ->
+            if(!content.contains(textToAttribute.key)) return@forEach
+            _attributes.add(textToAttribute.value)
+        }
 
 }
